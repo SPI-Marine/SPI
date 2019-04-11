b0726e8c0935501b58b0c4b408d3ad51123adbe3
pitwick <bboswick@gmail.com>
2019-04-11 07:17:23 -0600

    Added Fact_FixtureBerthEvents for event duration calculation

---
+++ Facts/Staging/Staging.Fact_FixtureBerthEvents.sql
@@ -0,0 +1,39 @@
+/*
+==========================================================================================================
+Author:			Brian Boswick
+Create date:	04/08/2019
+Description:	Creates the Staging.Fact_FixtureBerthEvents table
+Changes
+Developer		Date		Change
+----------------------------------------------------------------------------------------------------------
+==========================================================================================================	
+*/
+
+drop table if exists Staging.Fact_FixtureBerthEvents;
+go
+
+create table Staging.Fact_FixtureBerthEvents
+	(
+		PostFixtureAlternateKey				int					not null,
+		ParcelBerthAlternateKey				int					not null,
+		LoadDischarge						varchar(50)			null,
+		EventNum							smallint			not null,
+		EventTypeId							int					null,
+		EventName							varchar(50)			null,
+		NextEventTypeId						int					null,
+		NextEventName						varchar(50)			null,
+		StartDateTime						datetime			null,
+		Duration							decimal(18, 4)		null,
+		LayTimeUsedProrated					decimal(18, 4)		null,
+		IsPumpingTime						char(1)				null,
+		IsLayTime							char(1)				null,
+		constraint [PK_Staging_Fact_FixtureBerthEvents_AltKeys] primary key clustered 
+		(
+			PostFixtureAlternateKey,
+			ParcelBerthAlternateKey,
+			EventNum asc
+		)
+			with 
+				(pad_index = off, statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) 
+			on [primary]
+	) on [primary];
