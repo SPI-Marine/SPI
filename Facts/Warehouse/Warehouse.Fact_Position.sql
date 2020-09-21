3b9a622264eac39be6962be2bcbfb034cb6ddca9
Brian Boswick <BrianB@csgpro.com>
2020-09-21 15:37:43 -0400

    Changed OwnerKey to OwnerParentKey and repointed to Dim_OwnerParent in Fact_Position

--- Facts/Warehouse/Warehouse.Fact_Position.sql
+++ Facts/Warehouse/Warehouse.Fact_Position.sql
@@ -8,6 +8,7 @@ Developer		Date		Change
 ----------------------------------------------------------------------------------------------------------
 Brian Boswick	02/10/2020	Added OwnerKey ETL logic
 Brian Boswick	02/12/2020	Added Direction and ShippingArea
+Brian Boswick	09/21/2020	Changed OwnerKey to OwnerParentKey
 ==========================================================================================================	
 */
 
@@ -24,7 +25,7 @@ create table Warehouse.Fact_Position
 		VesselKey								int					not null,
 		OpenDateKey								int					not null,
 		EndDateKey								int					not null,
-		OwnerKey								int					not null,
+		OwnerParentKey							int					not null,
 		Comments								varchar(500)		null,		-- Degenerate Dimension Attributes
 		StatusCalculation						varchar(500)		null,
 		LastCargo								varchar(500)		null,
